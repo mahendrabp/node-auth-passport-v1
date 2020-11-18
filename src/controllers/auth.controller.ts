540121cdbd64b44ea1c3115b972c11d210a155ae
mahendrabp <chipxitro@gmail.com>
2020-11-18 17:08:47 +0700

    Add: login feature

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -23,3 +23,27 @@ export const register = async (
   await newUser.save();
   return res.status(201).json(newUser);
 };
+
+export const login = async (req: Request, res: Response) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({
+      msg: 'fill your email and password',
+    });
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).json({
+      msg: 'user does not exist!',
+    });
+  }
+
+  const isMatching = await user.comparePassword(req.body.password);
+  if (isMatching) {
+    res.status(200).json(user);
+  }
+
+  return res.status(400).json({
+    msg: 'email or password wrong!',
+  });
+};
