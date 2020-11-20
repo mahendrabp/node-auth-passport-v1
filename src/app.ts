8e6565942420894cbb90654c051f76a0872002ca
mahendrabp <chipxitro@gmail.com>
2020-11-20 23:21:25 +0700

    Add: implement passport middleware in app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import { config as dontenv } from 'dotenv';
 import { RouteNav } from './routes';
+import passport from 'passport';
+import passportMiddleware from './middlewares/passport';
 
 //initialization
 const app = express();
@@ -14,6 +16,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(passport.initialize());
+passport.use(passportMiddleware);
+
+// routes
 app.use('/api/v1', RouteNav);
 
 export default app;
